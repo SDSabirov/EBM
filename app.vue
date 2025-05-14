e703ab40bea7e7e3de27f11d6a7e950d25608412
Saidmurod Sabirov <sabir.dev7@gmail.com>
2025-05-14 11:42:53 +0100

    instagram telgram links

--- app.vue
+++ app.vue
@@ -1,29 +1,75 @@
 <template>
   <div
     class="relative min-h-screen bg-repeat bg-center"
-    style="background-image: url('/bg-pattern.png');"
+    style="background-image: url('/bg-pattern.png')"
   >
     <!-- Overlay -->
     <div class="absolute inset-0 bg-black bg-opacity-50"></div>
 
     <!-- Centered Content -->
-    <div class="relative z-10 flex flex-col items-center justify-center h-screen px-4">
+    <div
+      class="relative z-10 flex flex-col items-center justify-center h-screen px-4"
+    >
       <!-- Logo -->
-      <img src="/logowhite.png" alt="Logo" class="h-64  mb-6" />
+      <img src="/logowhite.png" alt="Logo" class="h-64 mb-6" />
 
       <!-- Headline -->
       <h1 class="text-5xl font-extrabold text-white mb-2">Coming Soon</h1>
 
       <!-- Subtext -->
       <p class="text-lg text-white text-center max-w-md">
-        We're working hard behind the scenes to bring you something amazing. Stay tuned!
+        We're working hard behind the scenes to bring you something amazing.
+        Stay tuned!
       </p>
+      <!-- Social Icons -->
+      <div class="flex space-x-6 mt-6">
+        <a
+          href="https://www.instagram.com/ebm_clinic/?hl=en"
+          target="_blank"
+          rel="noopener"
+          class="hover:opacity-80 text-white"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <!--Boxicons v3.0 https://boxicons.com | License  https://docs.boxicons.com/free-->
+            <path
+              d="M11.999 7.377a4.623 4.623 0 1 0 0 9.248 4.623 4.623 0 0 0 0-9.248m0 7.627a3.004 3.004 0 1 1 0-6.008 3.004 3.004 0 0 1 0 6.008M16.806 6.129a1.078 1.078 0 1 0 0 2.156 1.078 1.078 0 1 0 0-2.156"
+            ></path>
+            <path
+              d="M20.533 6.111A4.6 4.6 0 0 0 17.9 3.479a6.6 6.6 0 0 0-2.186-.42c-.963-.042-1.268-.054-3.71-.054s-2.755 0-3.71.054a6.6 6.6 0 0 0-2.184.42 4.6 4.6 0 0 0-2.633 2.632 6.6 6.6 0 0 0-.419 2.186c-.043.962-.056 1.267-.056 3.71s0 2.753.056 3.71c.015.748.156 1.486.419 2.187a4.6 4.6 0 0 0 2.634 2.632 6.6 6.6 0 0 0 2.185.45c.963.042 1.268.055 3.71.055s2.755 0 3.71-.055a6.6 6.6 0 0 0 2.186-.419 4.61 4.61 0 0 0 2.633-2.633c.263-.7.404-1.438.419-2.186.043-.962.056-1.267.056-3.71s0-2.753-.056-3.71a6.6 6.6 0 0 0-.421-2.217m-1.218 9.532a5 5 0 0 1-.311 1.688 2.99 2.99 0 0 1-1.712 1.711 5 5 0 0 1-1.67.311c-.95.044-1.218.055-3.654.055-2.438 0-2.687 0-3.655-.055a5 5 0 0 1-1.669-.311 2.99 2.99 0 0 1-1.719-1.711 5.1 5.1 0 0 1-.311-1.669c-.043-.95-.053-1.218-.053-3.654s0-2.686.053-3.655a5 5 0 0 1 .311-1.687c.305-.789.93-1.41 1.719-1.712a5 5 0 0 1 1.669-.311c.951-.043 1.218-.055 3.655-.055s2.687 0 3.654.055a5 5 0 0 1 1.67.311 3 3 0 0 1 1.712 1.712 5.1 5.1 0 0 1 .311 1.669c.043.951.054 1.218.054 3.655s0 2.698-.043 3.654z"
+            ></path>
+          </svg>
+        </a>
+        <a
+          href="https://t.me/ebm_clinicuz"
+          target="_blank"
+          rel="noopener"
+          class="hover:opacity-80 text-white"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+          >
+            <!--Boxicons v3.0 https://boxicons.com | License  https://docs.boxicons.com/free-->
+            <path
+              d="m20.665 3.717-17.73 6.837c-1.21.486-1.203 1.161-.222 1.462l4.552 1.42 10.532-6.645c.498-.303.953-.14.579.192l-8.533 7.701h-.002l.002.001-.314 4.692c.46 0 .663-.211.921-.46l2.211-2.15 4.599 3.397c.848.467 1.457.227 1.668-.785l3.019-14.228c.309-1.239-.473-1.8-1.282-1.434"
+            ></path>
+          </svg>
+        </a>
+      </div>
     </div>
   </div>
 </template>
 <script setup lang="ts">
 // Set page title
-
 </script>
 
 <style scoped>
